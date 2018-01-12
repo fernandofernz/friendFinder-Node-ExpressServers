31b157ac5a891f4b276d6c3c4626c767086a5611
Ferny Dany <fernydany@fernys-mbp.attlocal.net>
2018-01-11 17:48:50 -0800

    Catch all route added

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,8 +2,9 @@
 var path = require('path');
 
 //ROUTING
-module.exports = function(app){
 
+module.exports = function(app){
+  
   //default GET route that leads to home.html - displays home page
   app.get('/survey', function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/survey.html'));
@@ -12,5 +13,10 @@ module.exports = function(app){
   //a USE route to home page
   app.use(function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/home.html'));
+
+  // If no matching route is found default to home
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
   });
 };
